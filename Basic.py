26241cd312deef83c20e25cadf3dd98164ff9caf
meetashara31 <92880405+meetashara31@users.noreply.github.com>
2022-06-12 00:50:22 +0530

    Basic
    Start Learning

---
+++ Basic.py
@@ -0,0 +1,21 @@
+# Print Hello
+print("hello")
+
+#Multiple line comment
+
+"""dbibufvbisuvb
+
+    hbfuegfuyefue"""
+
+# Declare Variable
+a=5
+b=6
+
+# Print variable
+print(a)
+print(b)
+
+#Addision of variable
+print(a+b)
+
+
