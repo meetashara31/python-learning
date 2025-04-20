31db174edf6c1a3d6246e1ee2cd746d505d1ceb2
Meet Ashara <92880405+meetashara31@users.noreply.github.com>
2025-04-20 22:59:20 +0530

    Create interview.py

---
+++ interview.py
@@ -0,0 +1,76 @@
+# interview purpose
+
+        # 1.1 Data Types & Structures
+        # 💡 Key Concepts:
+        # int, float, str, bool
+
+        # list, tuple, dict, set
+
+# num = 4
+# point = 4.5
+# name = 'Meet'
+# result = True
+
+# ls = ['meet','hello',False,105,5.5,1]
+# tup = ('tuple','meet','hello',False,5,5.5,99)
+# json = {
+#     'FirstName':'meet',
+#     'LastName':'ashara',
+#     'age':222,
+#     'height':6.4,
+# }
+
+
+
+# print(f'this is an tuple :- {tup}')
+
+# for i in json:
+#     print(f'{i} : {json[i]}')
+    
+# pat = {x:x*2 for x in range(1,11)}
+
+# for x in pat:
+#     print(f' {x} => {pat[x]} ')
+
+# aset = {1,2,3,4,4}
+# aset1 = {1,2,5,4,6}
+
+# print(aset1 - aset)
+
+# p='hello'
+# count=0
+# for i in p:
+#     print(i)
+#     count+=1
+# print(count)
+
+
+# def demo(names):
+#     update = set(names)
+#     up = tuple(update)
+#     for i in up:
+#         dis={i:len(i)}
+#         print(dis)
+        
+#         for key, value in dis.items():
+#             print(key, value)
+
+    
+# demo(['maeet','meet','meet','ashara','good'])
+
+    
+# def user_report(name,age,skill,**info):
+#     print(f'name : {name}')
+#     print(f'age : {age}')
+#     print(f'skills : {skill}')
+#     for key , value in info.items():
+#         print(f'{key}:{value}')
+# dis={
+#     ' city':' Rajkot',
+#     'email': 'meet@gmail.com'
+# }
+# skill=("python , djnaog, js")
+# user_report('meet',20,skill,dis)
+    
+
+
